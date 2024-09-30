ba1c6b1bbbfd114ee06bbcb961d38298d37d7f56
Yurapon Tinlapat <you@example.com>
2024-09-30 13:42:54 +0700

    feat:done ex1 ex2 ex3 ex4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstname = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const shopName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const openShopNum = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+openShopNum = 2500;
